0faf903ff9c58f7f7914c998cfff83f1c825e9bf
seiobata <s.bautista18@yahoo.com>
2025-07-06 11:15:04 -0500

    add unit testing

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello, World!\n")
+}
+
+func cleanInput(text string) []string {
+	lower_text := strings.ToLower(text)
+	new_text := strings.Fields(lower_text)
+	return new_text
 }
---
+++ repl_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Squirtle CHARMANDER bulbaSAUR",
+			expected: []string{"squirtle", "charmander", "bulbasaur"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Error: length of slice does not match")
+			t.FailNow()
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Error: word does not match")
+				t.FailNow()
+			}
+		}
+	}
+}
